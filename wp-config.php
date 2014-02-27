cb46de2dd455126e59c7f4d9e34b42aa944c3f4d
Lee Williams <lant@lantius.org>
2014-02-27 00:48:00 -0800

    Cleanup and remove redundancies from wp-config.

--- wp-config.php
+++ wp-config.php
@@ -7,48 +7,35 @@
 * Note: Define them all, don't skip one if other is already defined
 */
 
-define( 'DB_CREDENTIALS_PATH', dirname( ABSPATH ) ); // cache it for multiple use
-define( 'WP_LOCAL_SERVER', file_exists( DB_CREDENTIALS_PATH . '/local-config.php' ) );
-define( 'WP_DEV_SERVER', file_exists( DB_CREDENTIALS_PATH . '/dev-config.php' ) );
-define( 'WP_STAGING_SERVER', file_exists( DB_CREDENTIALS_PATH . '/staging-config.php' ) );
+define('DB_CREDENTIALS_PATH', dirname(ABSPATH)); // cache it for multiple use
+define('WP_LOCAL_SERVER', file_exists(DB_CREDENTIALS_PATH . '/local-config.php'));
+define('WP_DEV_SERVER', file_exists(DB_CREDENTIALS_PATH . '/dev-config.php'));
 
 /**
 * Load DB credentials
 */
 
-if ( WP_LOCAL_SERVER )
-	require DB_CREDENTIALS_PATH . '/local-config.php';
-elseif ( WP_DEV_SERVER )
-	require DB_CREDENTIALS_PATH . '/dev-config.php';
-elseif ( WP_STAGING_SERVER )
-	require DB_CREDENTIALS_PATH . '/staging-config.php';
-else
-	require DB_CREDENTIALS_PATH . '/production-config.php';
+if (WP_LOCAL_SERVER) {
+  require DB_CREDENTIALS_PATH . '/local-config.php';
+} elseif (WP_DEV_SERVER) {
+  require DB_CREDENTIALS_PATH . '/dev-config.php';
+} else {
+  require DB_CREDENTIALS_PATH . '/production-config.php';
+}
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
 
 /**
-* Authentication Unique Keys and Salts.
-*
-* Change these to different unique phrases!
-* You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
-* You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
-*/
-
-if ( ! defined( 'AUTH_KEY' ) )
-	define('AUTH_KEY', '9*W=5&lt;Rw-)c].9}g?^[:!j]h+Efr&lt;y$&lt;YmV0XOo|lOIujEE}+[R}iAQZ :Sy3wN}');
-if ( ! defined( 'SECURE_AUTH_KEY' ) )
-	define('SECURE_AUTH_KEY', 'APge3~H;g+b0FyNF&amp;e`$=g?qj9@FQwqFe^Q4(@p#kDa=NR? $Z9|@v*a(tOj*B+.');
-if ( ! defined( 'LOGGED_IN_KEY' ) )
-	define('LOGGED_IN_KEY', '5l0+:WTpj8#[V|;&lt;Iw;%rkB(A}r++HwT|s[LW!.wt.=5J!b%Z{F1/[LxQ*d7J&gt;Cm');
-if ( ! defined( 'NONCE_KEY' ) )
-	define('NONCE_KEY', 'zO2cmQX`Kc~_XltJR&amp;T !Uc72=5Cc6`SxQ3;$f]#J)p&lt;/wwX&amp;7RTB2)K1Qn2Y*c0');
-if ( ! defined( 'AUTH_SALT' ) )
-	define('AUTH_SALT', 'je]#Yh=RN DCrP9/N=IX^,TWqvNsCZJ4f7@3,|@L]at .-,yc^-^+?0ZfcHjD,WV');
-if ( ! defined( 'SECURE_AUTH_SALT' ) )
-	define('SECURE_AUTH_SALT', '^`6z+F!|+$BmIp&gt;y}Kr7]0]Xb@&gt;2sGc&gt;Mk6,$5FycK;u.KU[Tw$345K9qoF}WV,-');
-if ( ! defined( 'LOGGED_IN_SALT' ) )
-	define('LOGGED_IN_SALT', 'a|+yZsR-k&lt;cSf@PQ~v82a_+{+hRCnL&amp;|aF|Z~yU&amp;V0IZ}Mrz@ND])YD22iUM[%Oc');
-if ( ! defined( 'NONCE_SALT' ) )
-	define('NONCE_SALT', '|1.e9Tx{fPv8D#IXO6[&lt;WY*,)+7+URp0~|:]uqiCOzu93b8,h4;iak+eIN7klkrW');
+ * Paths
+ */
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wordpress');
+define('WP_HOME',    'http://' . $_SERVER['SERVER_NAME']);
+define('WP_CONTENT_DIR', $_SERVER['DOCUMENT_ROOT'] . '/wp-content');
+define('WP_CONTENT_URL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp-content');
 
 /**
 * WordPress Database Table prefix.
@@ -57,7 +44,7 @@ if ( ! defined( 'NONCE_SALT' ) )
 * prefix. Only numbers, letters, and underscores please!
 */
 
-$table_prefix = 'ft_';
+$table_prefix = 'wp_';
 
 /**
 * WordPress Localized Language, defaults to English.
@@ -78,31 +65,21 @@ define( 'WPLANG', '' );
 * in their development environments.
 */
 
-if ( WP_LOCAL_SERVER || WP_DEV_SERVER ) {
-
-	define( 'WP_DEBUG', true );
-	define( 'WP_DEBUG_LOG', true ); // Stored in wp-content/debug.log
-	define( 'WP_DEBUG_DISPLAY', true );
-
-	define( 'SCRIPT_DEBUG', true );
-	define( 'SAVEQUERIES', true );
-
-} else if ( WP_STAGING_SERVER ) {
-
-	define( 'WP_DEBUG', true );
-	define( 'WP_DEBUG_LOG', true ); // Stored in wp-content/debug.log
-	define( 'WP_DEBUG_DISPLAY', false );
-
+if (WP_LOCAL_SERVER || WP_DEV_SERVER) {
+  define('WP_DEBUG', true);
+  define('WP_DEBUG_LOG', true); // Stored in wp-content/debug.log
+  define('WP_DEBUG_DISPLAY', true);
+  define('SCRIPT_DEBUG', true);
+  define('SAVEQUERIES', true);
 } else {
-
-	define( 'WP_DEBUG', false );
+  define('WP_DEBUG', false);
 }
 
 
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
+if (!defined('ABSPATH'))
 define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
