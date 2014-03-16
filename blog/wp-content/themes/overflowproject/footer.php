1120879e0beadefb3fd1a761db0ede083bb764a4
Lee Williams <lant@lantius.org>
2014-03-16 01:59:34 -0700

    Performance optimizations.

--- blog/wp-content/themes/overflowproject/footer.php
+++ blog/wp-content/themes/overflowproject/footer.php
@@ -14,13 +14,13 @@
 </div>
 
 <script src="/js/jquery.min.js"></script>
-<script src="../lib/fitvids/jquery.fitvids.js"></script>
-<script src="/js/rotate-options.js"></script>
-<script src="/js/jquery.dropotron.min.js"></script>
-<script src="/js/jquery.scrolldepth.min.js"></script>
 <script src="/js/config.js"></script>
 <script src="/js/skel.min.js"></script>
 <script src="/js/skel-panels.min.js"></script>
+<script src="/lib/fitvids/jquery.fitvids.js"></script>
+<script src="/js/rotate-options.js"></script>
+<script src="/js/jquery.dropotron.min.js"></script>
+<script src="/js/jquery.scrolldepth.min.js"></script>
 <script>
   $(document).ready(function(){
     // Target your .container, .wrapper, .post, etc.
